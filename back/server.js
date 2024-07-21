e61916bb0542d293b7faf90efeeb4a748d064b42
Niketan100 <niketankumar12345@gmail.com>
2024-07-21 17:07:58 +0530

    deployment prep

--- back/server.js
+++ back/server.js
@@ -7,6 +7,7 @@ import postRoutes from './routes/post.routes.js'
 import notificationRoutes from './routes/notification.routes.js'
 import cookieParser from 'cookie-parser';
 import {v2 as cloudinary}   from 'cloudinary'
+import path from 'path';
 
 import cors from 'cors'
 dotenv.config();
@@ -21,14 +22,9 @@ api_secret : process.env.CLOUDINARY_API_SECRET
 
 const app = express();
 
-app.use(cors({
-    origin : 'http://localhost:5173',
-    credentials : true,
-    
-}))
+const port = process.env.port || 3000;
+const __dirname = path.resolve();
 
-
-const port = 3000;
 
 app.use(express.json({limit : "10mb"}));
 app.use(express.urlencoded({ extended: true }));
@@ -39,6 +35,14 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
 
+
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static(path.join(__dirname, '/front/dist')));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, '/front/dist/index.html'));
+    })
+}
 
 
 app.listen(port , () =>{
