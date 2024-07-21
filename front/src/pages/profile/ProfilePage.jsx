f9d08bcd9eed31ac4d94ea8a0dbe4c8880163a6b
Niketan100 <niketankumar12345@gmail.com>
2024-07-21 17:30:25 +0530

    changes_made

--- front/src/pages/profile/ProfilePage.jsx
+++ front/src/pages/profile/ProfilePage.jsx
@@ -201,12 +201,12 @@ const ProfilePage = () => {
 											<>
 												<FaLink className='w-3 h-3 text-slate-500' />
 												<a
-													href='https://youtube.com/@asaprogrammer_'
+													href='https://twitter-1-p4kx.onrender.com'
 													target='_blank'
 													rel='noreferrer'
 													className='text-sm text-blue-500 hover:underline'
 												>
-													youtube.com/@asaprogrammer_
+													{user?.link}
 												</a>
 											</>
 										</div>
